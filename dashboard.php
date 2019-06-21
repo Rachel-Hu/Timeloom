e9b496665e6fba24e9f9c87393626f2782d6f07e
Ruiqi Hu <ruiqih@andrew.cmu.edu>
2019-06-20 21:36:58 -0400

    adjust code format

--- dashboard.php
+++ dashboard.php
@@ -232,7 +232,7 @@
                                         </div>
                                         <div class="modal-footer">
                                             <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> -->
-                                            <button type="submit" class="btn btn-primary edit-submit-btn" name="edit-submit-btn" value="">Edit</button>
+                                            <button type="submit" class="btn btn-primary edit-submit-btn" name="edit-submit-btn" value="">Save</button>
                                         </div>
                                     </form>
                                 </div>
