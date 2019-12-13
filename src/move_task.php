edd06592e24ec1066722c1cb3301b7b0f812130e
Ruiqi Hu <ruiqih@andrew.cmu.edu>
2019-12-13 00:35:31 -0500

    autofill 'Done by' when it is empty and moved to 'completed' list

--- src/move_task.php
+++ src/move_task.php
@@ -9,6 +9,7 @@
         $prev_list = $_GET['prev'];
         print_r($_GET);
         $ids = explode('_', $id_str);
+        // Expired to active
         if($prev_list == 4 && $list == 2) {
             foreach ($ids as $id) {
                 // Get properties
@@ -19,13 +20,40 @@
                 }
                 $task = mysqli_fetch_assoc($task_result);
                 $properties = json_decode($task['properties']);
-                // print_r($properties);
                 $index = array_search("Due Date", array_column($properties, "name"));
                 // Set new time
                 $current = date("Y-m-d\TH:i");
                 $default = date("Y-m-d\TH:i", strtotime('+2 day', strtotime($current)));
                 $due_date = (array)$properties[$index];
                 $due_date['value'] = $default;
+                $properties[$index] = $due_date;
+                $properties = json_encode($properties);
+                $query = "UPDATE task SET task_list_id = $list, properties = '$properties' WHERE id = $id ";
+                echo $query;
+                $move_task_query = mysqli_query($connect, $query);
+                if(!$move_task_query) {
+                    die("QUERY FAILED ".mysqli.error($connect)).' '.msqli_errno($connect);
+                }
+            }
+        }
+        // Move to completed
+        else if($list == 3) {
+            // Autofill done by if necessary
+            foreach ($ids as $id) {
+                // Get properties
+                $task_query = "SELECT * FROM task WHERE id = $id";
+                $task_result = mysqli_query($connect, $task_query);
+                if(!$task_result) {
+                    die("QUERY FAILED ".mysqli.error($connect)).' '.msqli_errno($connect);
+                }
+                $task = mysqli_fetch_assoc($task_result);
+                $properties = json_decode($task['properties']);
+                $index = array_search("Done by", array_column($properties, "name"));
+                // Set new time
+                $current = date("Y-m-d\TH:i");
+                $due_date = (array)$properties[$index];
+                if($due_date['value'] == '') 
+                    $due_date['value'] = $current;
                 $properties[$index] = $due_date;
                 $properties = json_encode($properties);
                 $query = "UPDATE task SET task_list_id = $list, properties = '$properties' WHERE id = $id ";
